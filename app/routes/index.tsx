b9afdfcb7cd9f67cebc5e95143e4a83216a64e71
wn <csignore@whril.com>
2024-01-29 00:08:33 -0500

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+import logo from "~/../public/img/ja.png";
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="inner-container">
+        <img src={logo} />
+      </div>
+    </div>
+  );
+}
