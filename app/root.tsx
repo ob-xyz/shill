763a21918753def266dcba04b441795fffa3c08c
wn <csignore@whril.com>
2024-02-12 14:48:10 -0500

    1

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shill",
-  description: "Y'know you wanna.",
+  description: "Y'know you make me wanna.",
   viewport: "width=device-width,initial-scale=1",
 });
 
