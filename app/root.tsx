e41bdaa9731c996da9f884c1ee8079b94127c7bd
wn <csignore@whril.com>
2024-05-22 11:23:31 -0400

    x

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shill",
-  description: "A better YouTube.", 
+  description: "A new way to shill.",
   viewport: "width=device-width,initial-scale=1",
 });
 
