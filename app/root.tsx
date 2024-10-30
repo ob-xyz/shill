d8c8c0e4c4146b3ae37e5b41e3a3b9e11dd009fa
wn <csignore@whril.com>
2024-10-30 10:15:46 -0400

    xx

--- app/root.tsx
+++ app/root.tsx
@@ -27,7 +27,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Shill: #1 B2B Sales System",
+  title: "Shill: #1 Direct Advertising Sales System",
   description: "Shill eliminates the painful parts of being a content creator, combining insert orders, invoices, payments, campaign reports, and insights into automated emails.",
   viewport: "width=device-width,initial-scale=1",
 });
