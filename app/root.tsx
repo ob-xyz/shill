eacee9bfc019a08c6c732a6b94c09c2091772c1d
wn <csignore@whril.com>
2024-12-10 14:21:49 -0500

    xx

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Shill",
-  description: "Shill is an alternative to Amazon and Shopify.",
+  description: "Shill delivers innovative products, experiences and services to inspire entrepreneurs like me and you.",
   viewport: "width=device-width,initial-scale=1",
 });
 
