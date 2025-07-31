8da362d1e60d799b334bfc29823d73689235b87e
wn <45404661+ob-xyz@users.noreply.github.com>
2025-07-31 14:29:08 -0400

    11

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,22 @@ import react from "@vitejs/plugin-react";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     reactRouter(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "_redirects",
+          dest: "."
+        }
+      ]
+    })
   ],
   build: {
     outDir: "build/client",
