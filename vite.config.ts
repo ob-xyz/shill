672239e948c8da97f06fc8c955c3c1173d2f4b47
wn <45404661+ob-xyz@users.noreply.github.com>
2025-07-31 13:54:25 -0400

    ////

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths()
+  ],
+  build: {
+    outDir: "dist"
+  }
 });
