4224539c2331e7dfa38f6fd001e52bc9be16ee45
wn <45404661+ob-xyz@users.noreply.github.com>
2025-09-28 02:32:42 -0400

    178

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -24,7 +24,7 @@ export default function About() {
         <img src={logo} alt="Shilll Logo" />
       </div>
       <div className="content-aboutus2">
-        <h2>Humans making <span>content</span> for humans.</h2>
+        <h2>A highly <span>autonomous</span> platform.</h2>
         <p>161 Bobs Lake Lane 11</p>
         <p>Tay Valley, Ontario</p>
         <p>613-273-7555</p>
@@ -33,11 +33,11 @@ export default function About() {
   <img src={logo} alt="Logo" />
 <div className="footer-top">
   <div className="footer-signup">
-    <p>Subscribe to know more.</p>
+    <p>Screw it, let's do it.</p>
     <form method="post" action="https://app.jeffamzn.com/subscription/form">
       <div className="input-wrapper">
         <input className="email" type="email" name="email" required placeholder="Email Address *" />
-        <button className="submit" type="submit">Sign up</button>
+        <button className="submit" type="submit">Let's do it</button>
       </div>
       <div className="h-captcha" data-sitekey="7e96e6a6-eef8-4624-be9c-e468b5a8b230"></div>
       <input id="6d48f" type="hidden" name="l" value="6d48fffe-7d37-4c14-b317-3e4cda33a647" />
