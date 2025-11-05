d42707c463b49a085f29db4696e40e3f046160d6
wn <45404661+ob-xyz@users.noreply.github.com>
2025-11-05 18:13:41 -0500

    1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
       <div className="footer-top">
         <div className="footer-signup">
           <h1>Screw it, let's do it.</h1>
-          <p>Make smarter decisions with your money.</p>
+          <p>Make smarter moves with your money.</p>
           <form method="post" action="https://app.jeffamzn.com/subscription/form">
             <div className="input-wrapper">
               <input className="email" type="email" name="email" required placeholder="Email Address *" />
