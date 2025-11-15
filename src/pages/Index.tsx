908a5654abe24787aad88f60f838cb369d443f49
wn <45404661+ob-xyz@users.noreply.github.com>
2025-11-15 02:43:42 -0500

    1

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,8 +26,8 @@ export default function Index() {
       <div className="content">
         <div className="inner-content">
           <div className="text">
-            <h4>WELCOME TO SHILLL</h4>
-            <h2><span>We're a Canadian company who develops and builds new companies that last for generations.</span></h2>
+            <h4>WE INVENT AND ACQUIRE PRODUCTS AND BRANDS</h4>
+            <h2><span>We're here to move the world with new companies.</span></h2>
             <p>Sign up to stay informed.</p>
           </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
